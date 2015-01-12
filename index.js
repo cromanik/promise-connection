3bd670d9dd11fae2b722e8fb8d50bd63d115af93
Corey Frang <gnarf@gnarf.net>
2015-01-12 12:35:13 -0500

    Added connection workflow

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var UUID = require('./lib/uuid');
+var object = require('./lib/object');
 var PC = module.exports = {
   assert: require('./lib/assert'),
+  extend: object.extend,
   Promise: undefined,
   UUID: UUID,
 };
--- test/index.js
+++ test/index.js
@@ -1,13 +1,17 @@
 var expect = chai.expect;
 
-function MockPort() {}
+function MockPort() {
+  this.postMessage = sinon.spy();
+}
 
-MockPort.prototype.postMessage = function() {};
+MockPort.prototype.recvmock = function(data) {
+  this.onmessage({ data: promiseConnection.extend({ _pc: true } , data) });
+};
+
 MockPort.prototype.onmessage = function() {};
 
 describe('promiseConnection', function() {
   beforeEach(function() {
-    // ensure Promise is defined for every test
     expect(window.Promise).to.be.a('function');
     promiseConnection.Promise = window.Promise;
   });
@@ -16,7 +20,7 @@ describe('promiseConnection', function() {
     expect(promiseConnection).to.be.ok;
   });
 
-  describe("Connection", function() {
+  describe(".Connection()", function() {
     var Connection = promiseConnection.Connection;
 
     it('is a function', function() {
@@ -28,6 +32,157 @@ describe('promiseConnection', function() {
         delete promiseConnection.Promise;
         new Connection(new MockPort(), {})
       }).to.throw('promiseConnection.Promise must be defined');
+    });
+
+    describe('opts: { autoConnect: false }', function() {
+      var port, local, options, connection;
+      beforeEach(function() {
+        port = new MockPort();
+        local = {};
+        options = {
+          autoConnect: false
+        };
+        connection = new Connection(port, local, options)
+      });
+
+      it('does not send any message on creation with autoConnect: false', function() {
+        expect(port.postMessage.called).to.be.false;
+      });
+
+      describe('.connect()', function() {
+        beforeEach(function() {
+          // the promise returned by connect should always equal "connected"
+          expect(connection.connect()).to.equal(connection.connected);
+        }); 
+
+        it('sends a connect message when you call .connect()', function() {
+          expect(port.postMessage.callCount).to.equal(1);
+          var messageArgs = port.postMessage.args[0];
+          expect(messageArgs[0]._id).to.equal('connect');
+          expect(messageArgs[0]._type).to.equal('connect');
+        });
+
+        it('resolves connection when remote responds', function() {
+          expect(connection._connecting).to.be.ok;
+          expect(connection._connected).to.not.exist;
+          port.recvmock({ _type: 'resolve', _id: 'connect', value: true })
+          return connection.connected.then(function() {
+            expect(connection._connecting).to.not.exist;
+            expect(connection._connected).to.be.ok;
+          });
+        });
+
+        it('resends connection packet when you call .connect() a second time', function() {
+          // the promise returned by connect should always equal "connected"
+          var originalArgs = port.postMessage.args[0];
+          port.postMessage.reset();
+
+          expect(connection.connect()).to.equal(connection.connected);
+          expect(port.postMessage.callCount).to.equal(1);
+          expect(port.postMessage.args[0]).to.deep.equal(originalArgs);
+        });
+      });
+
+      describe('incoming connect', function() {
+        beforeEach(function() {
+          expect(connection._connecting).to.be.not.ok
+          port.recvmock({ _type: 'connect', _id: 'connect' });
+          // let promise resolvers tick before moving on to tests
+          return Promise.resolve();
+        });
+
+        it('sends its own connection', function() {
+          expect(port.postMessage.callCount).to.equal(1);
+          var data = port.postMessage.args[0][0];
+          expect(data._type).to.equal('connect');
+        });
+
+        it('becomes connected when remote replies', function() {
+          expect(connection._connecting).to.be.ok;
+          expect(connection._connected).to.not.exist;
+          port.postMessage.reset();
+          port.recvmock({ _type: 'resolve', _id: 'connect', value: true })
+          return connection.connected.then(function() {
+            expect(connection._connecting).to.not.exist;
+            expect(connection._connected).to.be.ok;
+
+            return Promise.resolve();
+          }).then(function() {
+            // and make sure it tells the other side it's connected
+            expect(port.postMessage.callCount).to.equal(1);
+            var data = port.postMessage.args[0][0];
+            expect(data._type).to.equal('resolve');
+            expect(data._id).to.equal('connect');
+
+          });
+        });
+      });
+
+    });
+
+    describe('opts: {}', function() {
+      var port, local, options, connection;
+      beforeEach(function() {
+        port = new MockPort();
+        local = {};
+        options = {};
+        connection = new Connection(port, local, options)
+      });
+
+      it('sends a connect message immediately', function() {
+        expect(port.postMessage.callCount).to.equal(1);
+        var messageArgs = port.postMessage.args[0];
+        expect(messageArgs[0]._id).to.equal('connect');
+        expect(messageArgs[0]._type).to.equal('connect');
+      });
+
+      describe('incoming connect', function() {
+        beforeEach(function() {
+          port.postMessage.reset();
+          expect(connection._connecting).to.be.ok;
+          port.recvmock({ _type: 'connect', _id: 'connect' });
+          // let promise resolvers tick before moving on to tests
+          return Promise.resolve();
+        });
+
+        it('resends its own connection', function() {
+          expect(port.postMessage.callCount).to.equal(1);
+          var data = port.postMessage.args[0][0];
+          expect(data._type).to.equal('connect');
+        });
+
+        it('becomes connected when remote replies', function() {
+          expect(connection._connecting).to.be.ok;
+          expect(connection._connected).to.not.exist;
+          port.postMessage.reset();
+          port.recvmock({ _type: 'resolve', _id: 'connect', value: true })
+          return connection.connected.then(function() {
+            expect(connection._connecting).to.not.exist;
+            expect(connection._connected).to.be.ok;
+            return Promise.resolve();
+          }).then(function() {
+            // and make sure it tells the other side it's connected
+            expect(port.postMessage.callCount).to.equal(1);
+            var data = port.postMessage.args[0][0];
+            expect(data._type).to.equal('resolve');
+            expect(data._id).to.equal('connect');
+          });
+        });
+      });
+
+    });
+  });
+
+  describe('.extend()', function() {
+    var fix1 = { fix1: true, fix2: false };
+    var fix2 = { fix2: true };
+    var expected = { fix1: true, fix2: true };
+
+    it('copies keys from multiple objects into one', function() {
+      var dest = {};
+      var result = promiseConnection.extend(dest, fix1, fix2);
+      expect(result).to.deep.equal(expected);
+      expect(result).to.equal(dest);
     });
   });
 
